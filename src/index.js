cd8c1a937e7b06e426fcb057e28dfe3e815efd54
Sahand Shoghi <sahand.shoghi@gmail.com>
2017-07-01 12:11:20 +1000

    Apollo configuration with graphql+subscription env vars

--- src/index.js
+++ src/index.js
@@ -3,7 +3,31 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import ApolloClient, { createNetworkInterface } from 'apollo-client'
+import { ApolloProvider } from 'react-apollo'
+import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws'
 
+const wsClient = new SubscriptionClient(process.env.REACT_APP_SUBSCRIPTION_API, { reconnect: true })
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const networkInterface = createNetworkInterface({ uri: process.env.REACT_APP_GRAPHQL_API })
+
+const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(networkInterface, wsClient)
+
+const apolloClient = new ApolloClient({
+  networkInterface: networkInterfaceWithSubscriptions,
+  dataIdFromObject: o => o.id
+})
+
+ReactDOM.render(
+  (
+    <ApolloProvider client={apolloClient}>
+      <Router>
+        <Route exact path='/' component={App} />
+      </Router>
+    </ApolloProvider>
+  ),
+  document.getElementById('root')
+)
+
 registerServiceWorker();
